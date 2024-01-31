a2e37c4f4599861f0b9512f1e730b72fed930c6f
wamzuu <113836506+wamzuu@users.noreply.github.com>
2024-01-30 20:43:12 -0500

    added refresh and imported results to txt file

--- website-scraping.py
+++ website-scraping.py
@@ -1,18 +1,31 @@
 from bs4 import BeautifulSoup
 from pip._vendor import requests
+import time
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 
-soup= BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-for job in jobs:
-    published_date = job.find('span', class_="sim-posted").span.text
-    if 'few' in published_date:
-        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
-        skills = job.find('span', class_='srp-skills').text.replace(' ','')
-        link = job.header.h2.a['href']
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+    soup= BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_="sim-posted").span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+            skills = job.find('span', class_='srp-skills').text.replace(' ','')
+            link = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f: 
+                    f.write(f'Company Name: {company_name.strip()} \n')
+                    f.write(f'Required Skills: {skills.strip()} \n')
+                    f.write(f'Link: {link}')
+                print(f'File Saved : {index}')
 
-        print(f'Company Name: {company_name.strip()}')
-        print(f'Required Skills: {skills.strip()}')
-        print(f'Link: {link}')
-        print('')
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        minutes = 1
+        print(f'Waiting {minutes} minutes...')
+        time.sleep(minutes * 60)
